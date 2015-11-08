b877c9f6bf87fd4ae6fb4849094438807dab5fa4
Alessandro Molina <alessandro.molina@axant.it>
2015-11-08 03:09:53 +0100

    Remove duplicated tests

--- tests/test_rest_json.py
+++ tests/test_rest_json.py
@@ -5,6 +5,7 @@ from webob import serialize_date
 
 import transaction
 
+
 class TestRestJsonEditCreateDelete(CrudTest):
     """Basic tests for POST, PUT & DELETE using urlencoded parameters and requesting for JSON responses"""
 
@@ -131,6 +132,7 @@ class TestRestJsonEditCreateDelete(CrudTest):
         movie = DBSession.query(Movie).first()
         assert movie is not None
 
+
 class TestRestJsonRead(CrudTest):
     """Basic tests for GET requests with JSON responses"""
 
@@ -230,155 +232,6 @@ class TestRestJsonRead(CrudTest):
         assert result['value']['name'] == actor.name
         assert result['value']['movie_title'] == movie_title
 
-
-class TestRestJsonReadDictified(CrudTest):
-    def controller_factory(self):
-        class MovieController(EasyCrudRestController):
-            model = Movie
-            pagination = {'items_per_page': 3}
-            json_dictify = True
-
-        class ActorController(EasyCrudRestController):
-            model = Actor
-            json_dictify = True
-
-        class RestJsonController(TGController):
-            movies = MovieController(DBSession)
-            actors = ActorController(DBSession)
-
-        return RestJsonController()
-
-    def setUp(self):
-        super(TestRestJsonReadDictified, self).setUp()
-        genre = Genre(name='action')
-        DBSession.add(genre)
-
-        actors = [Actor(name='James Who'), Actor(name='John Doe'), Actor(name='Man Alone')]
-        list(map(DBSession.add, actors))
-
-        DBSession.add(Movie(title='First Movie', genre=genre, actors=actors[:2]))
-        DBSession.add(Movie(title='Second Movie', genre=genre))
-        DBSession.add(Movie(title='Third Movie', genre=genre))
-        DBSession.add(Movie(title='Fourth Movie', genre=genre))
-        DBSession.add(Movie(title='Fifth Movie'))
-        DBSession.add(Movie(title='Sixth Movie'))
-        DBSession.flush()
-        transaction.commit()
-
-    def test_get_all(self):
-        result = self.app.get('/movies.json?order_by=movie_id')
-        result = result.json['value_list']
-        assert result['total'] == 6, result
-        assert result['page'] == 1, result
-        assert result['entries'][0]['title'] == 'First Movie', result
-        assert len(result['entries'][0]['actors']) == 2, result
-
-        result = self.app.get('/movies.json?page=2&order_by=movie_id')
-        result = result.json['value_list']
-        assert result['total'] == 6, result
-        assert result['page'] == 2, result
-        assert result['entries'][0]['title'] == 'Fourth Movie', result
-
-    def test_get_all_filter(self):
-        actor = DBSession.query(Actor).first()
-
-        result = self.app.get('/actors.json?movie_id=%s' % actor.movie_id)
-        result = result.json['value_list']
-        assert result['total'] == 2, result
-
-    def test_get_all___json__(self):
-        actor = DBSession.query(Actor).filter(Actor.movie_id!=None).first()
-        movie_title = actor.movie.title
-
-        result = self.app.get('/actors.json?movie_id=%s' % actor.movie_id)
-        result = result.json['value_list']
-        assert result['total'] > 0, result
-
-        for entry in result['entries']:
-            assert entry['movie_title'] == movie_title
-
-    def test_get_one(self):
-        movie = DBSession.query(Movie).first()
-        movie_actors_count = len(movie.actors)
-
-        result = self.app.get('/movies/%s.json' % movie.movie_id)
-        result = result.json
-        assert result['model'] == 'Movie', result
-        assert result['value']['title'] == movie.title
-        assert result['value']['movie_id'] == movie.movie_id
-        assert len(result['value']['actors']) == movie_actors_count
-
-    def test_get_one___json__(self):
-        actor = DBSession.query(Actor).filter(Actor.movie_id!=None).first()
-        movie_title = actor.movie.title
-
-        result = self.app.get('/actors/%s.json' % actor.actor_id)
-        result = result.json
-        assert result['model'] == 'Actor', result
-        assert result['value']['name'] == actor.name
-        assert result['value']['movie_title'] == movie_title
-
-class TestRestJsonEditCreateJsonBody(CrudTest):
-    def controller_factory(self):
-        class MovieController(EasyCrudRestController):
-            model = Movie
-
-        class RestJsonController(TGController):
-            movies = MovieController(DBSession)
-
-        return RestJsonController()
-
-    def test_post(self):
-        result = self.app.post_json('/movies.json', params={'title':'Movie Test'})
-
-        movie = DBSession.query(Movie).first()
-        assert movie is not None, result
-
-        assert movie.movie_id == result.json['value']['movie_id']
-
-    def test_post_validation(self):
-        result = self.app.post_json('/movies.json', params={'title':''}, status=400)
-        assert result.json['title'] is not None #there is an error for required title
-        assert result.json['description'] is None #there isn't any error for optional description
-
-        assert DBSession.query(Movie).first() is None
-
-    def test_put(self):
-        result = self.app.post_json('/movies.json', params={'title':'Movie Test'})
-        movie = result.json['value']
-
-        result = self.app.put_json('/movies/%s.json' % movie['movie_id'],
-                              params={'title':'New Title'})
-        assert result.json['value']['title'] == 'New Title'
-
-    def test_put_validation(self):
-        result = self.app.post_json('/movies.json', params={'title':'Movie Test'})
-        movie = result.json['value']
-
-        result = self.app.put_json('/movies/%s.json' % movie['movie_id'],
-                              params={'title':''}, status=400)
-        assert result.json['title'] is not None #there is an error for required title
-        assert result.json['description'] is None #there isn't any error for optional description
-
-        movie = DBSession.query(Movie).first()
-        assert movie.title == 'Movie Test'
-
-    def test_put_relationship(self):
-        result = self.app.post_json('/movies.json', params={'title':'Movie Test'})
-        movie = result.json['value']
-
-        actors = [Actor(name='James Who'), Actor(name='John Doe'), Actor(name='Man Alone')]
-        list(map(DBSession.add, actors))
-        DBSession.flush()
-        actor_ids = [actor.actor_id for actor in actors[:2]]
-        transaction.commit()
-
-        result = self.app.put_json('/movies/%s.json' % movie['movie_id'],
-                              params={'title':'Movie Test',
-                                      'actors':actor_ids})
-        assert len(result.json['value']['actors']) == 2, result
-
-        assert DBSession.query(Actor).filter_by(movie_id=movie['movie_id']).count() == 2
 
 class TestRestJsonConditionalPut(CrudTest):
     def controller_factory(self):
